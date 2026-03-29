b33a2a8ed1d065b834ff1ac58b0dc87010ed7bc9
SSumu <sadeepalsumudu@gmail.com>
2026-03-29 06:30:43 +0530

    HomeController.java was added

---
+++ medicine-search-backend/src/main/java/com/medicalsystem/medicine_search/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.medicalsystem.medicine_search.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running 🚀";
+    }
+}
