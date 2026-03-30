0a9da9611323716cf75d1dee853e3c0599a95623
SSumu <sadeepalsumudu@gmail.com>
2026-03-30 08:38:14 +0530

    InventoryController and application.properties were updated

--- medicine-search-backend/src/main/java/com/medicalsystem/medicine_search/controller/InventoryController.java
+++ medicine-search-backend/src/main/java/com/medicalsystem/medicine_search/controller/InventoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/inventory")
+@RequestMapping("/inventory")
 @CrossOrigin(origins = "*") // add your Vercel link here if needed
 @RequiredArgsConstructor
 public class InventoryController {
