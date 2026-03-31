9069631da8a5ff3b8b626b3b6f198662c459c17c
SSumu <sadeepalsumudu@gmail.com>
2026-04-01 00:36:36 +0530

    Some files were updated

--- medicine-search-backend/src/main/java/com/medicalsystem/medicine_search/controller/HomeController.java
+++ medicine-search-backend/src/main/java/com/medicalsystem/medicine_search/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/api")
+    @GetMapping("/")
     public String home() {
         return "Medicine Search Backend is Running Successfully!";
     }
