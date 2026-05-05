37cfbb5799fdfc4b212c251356ef53d09508025e
SSumu <sadeepalsumudu@gmail.com>
2026-05-05 07:53:22 +0530

    .gitignore file was updated

--- medicine-search-frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  // apiUrl: 'https://medicine-search-production.up.railway.app',
-  apiUrl: 'http://localhost:8080/api',
-  production: false,
-  googleMapsApiKey: 'AIzaSyCY8pHZvlHSOrnGWtk28s5OLjpT2jdWsLw',
-};
